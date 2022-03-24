19efd2e3c5eacf35edfb97c16e17d2f2df6aff17
sabbirzzaman <sztonmoy7@gmail.com>
2022-03-25 00:43:17 +0600

    set dynamic cart data

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,23 +3,31 @@ import './Cart.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
-const Cart = (props) => {
-    const { cart } = props;
+const Cart = ({ cart }) => {
+    let totalPrice = 0;
+    let totalShipping = 0;
 
-    console.log(cart);
+    for(const product of cart) {
+        totalPrice = product.price + totalPrice;
+        totalShipping = product.shipping + totalShipping;
+    }
+
+    const totalAmount = totalPrice + totalShipping;
+
+    const tax = (totalAmount * 5 / 100).toFixed(2);
 
     return (
         <div className="cart">
             <div>
                 <h2>Order Summary</h2>
                 <h3>Selected Items: {cart.length}</h3>
-                <h3>Total Price: {cart.length}</h3>
-                <h3>Total Shipping Charge: ${cart.length}</h3>
-                <h3>Tax: ${cart.length}</h3>
+                <h3>Total Price: ${totalPrice}</h3>
+                <h3>Total Shipping Charge: ${totalShipping}</h3>
+                <h3>Tax: ${tax}</h3>
             </div>
 
             <div>
-                <h2>Grand Total: ${cart.length}</h2>
+                <h2>Grand Total: ${totalPrice + totalShipping + parseFloat(tax)}</h2>
                 <button className="clear-cart">
                     Clear Cart <FontAwesomeIcon icon={faTrash} />
                 </button>
