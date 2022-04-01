3549c06653f8760304b8cff4d88d02ab6f1959ef
sabbirzzaman <sztonmoy7@gmail.com>
2022-04-01 22:42:23 +0600

    responsive reader

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import logo from '../../images/Logo.svg';
 import './Header.css';
@@ -6,16 +8,26 @@ import './Header.css';
 const Header = () => {
     const navigate = useNavigate();
 
-    return (
-        <div className='header'>
-            <nav className="header-container">
-            <img className='site-logo' onClick={() => navigate('/')} src={logo} alt="Site logo" />
+    const [open, setOpen] = useState(false);
 
-            <div>
-                <Link to="/home">Home</Link>
-                <Link to="/shop">Shop</Link>
-            </div>
-        </nav>
+    return (
+        <div className={open ? 'header' : 'header'}>
+            <nav className="header-container">
+                <img
+                    className="site-logo"
+                    onClick={() => navigate('/')}
+                    src={logo}
+                    alt="Site logo"
+                />
+
+                <div>
+                    <div className={open ? 'nav-menu hide-nav' : 'nav-menu'}>
+                        <Link to="/home">Home</Link>
+                        <Link to="/shop">Shop</Link>
+                    </div>
+                    <FontAwesomeIcon onClick={() => setOpen(!open)} icon={faBars}></FontAwesomeIcon>
+                </div>
+            </nav>
         </div>
     );
 };
