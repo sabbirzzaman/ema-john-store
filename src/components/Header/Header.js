d5d288d99b8541ab3171b332e3cccd26bd796bc7
sabbirzzaman <sztonmoy7@gmail.com>
2022-04-02 23:15:21 +0600

    custom active nav added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,30 @@
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useMatch, useNavigate, useResolvedPath } from 'react-router-dom';
 import logo from '../../images/Logo.svg';
 import './Header.css';
 
 const Header = () => {
     const navigate = useNavigate();
-
     const [open, setOpen] = useState(false);
+
+    function CustomLink({ children, to, ...props }) {
+        let resolved = useResolvedPath(to);
+        let match = useMatch({ path: resolved.pathname, end: true });
+      
+        return (
+          <div>
+            <Link
+              style={{ color: match ? "#ff9900" : "#FFF" }}
+              to={to}
+              {...props}
+            >
+              {children}
+            </Link>
+          </div>
+        );
+      }
 
     return (
         <div className={open ? 'header' : 'header'}>
@@ -20,10 +36,10 @@ const Header = () => {
                     alt="Site logo"
                 />
 
-                <div>
+                <div className='navigation'>
                     <div className={open ? 'nav-menu' : 'nav-menu hide-nav'}>
-                        <Link to="/home">Home</Link>
-                        <Link to="/shop">Shop</Link>
+                        <CustomLink to="/home">Home</CustomLink>
+                        <CustomLink to="/shop">Shop</CustomLink>
                     </div>
                     <FontAwesomeIcon onClick={() => setOpen(!open)} icon={faBars}></FontAwesomeIcon>
                 </div>
