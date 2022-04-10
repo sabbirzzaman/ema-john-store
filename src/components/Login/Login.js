e9e0813eb5722264357ba20c57aaaa79d333f979
sabbirzzaman <sztonmoy7@gmail.com>
2022-04-11 00:25:42 +0600

    functional signup and login hook

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,31 +1,57 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './Login.css';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    const [signInWithEmailAndPassword, user, loading, error] =
+        useSignInWithEmailAndPassword(auth);
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(email, password);
+    };
+
+    if(user) {
+        navigate('/')
+    }
+
     return (
         <div className="form-container">
             <div className="form-heading">
                 <h2>Login</h2>
             </div>
-            <form>
+            <form onSubmit={handleLogin}>
                 <div className="field-group">
                     <label htmlFor="email">Email</label>
                     <input
+                        onBlur={(e) => setEmail(e.target.value)}
                         type="email"
                         name="email"
                         placeholder="Enter your email"
                         id="email"
+                        required
                     />
                 </div>
                 <div className="field-group">
                     <label htmlFor="password">Password</label>
                     <input
+                        onBlur={(e) => setPassword(e.target.value)}
                         type="password"
                         name="password"
                         placeholder="Enter your password"
                         id="password"
+                        required
                     />
+                </div>
+                <div className="error">
+                    <p>{error?.message}</p>
                 </div>
                 <div className="field-group">
                     <input type="submit" value="Login" />
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,40 +1,76 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './SignUp.css';
 
 const SignUp = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [displayError, setDisplayError] = useState('');
+
+    const navigate = useNavigate();
+
+    const [createUserWithEmailAndPassword, user, error] =
+        useCreateUserWithEmailAndPassword(auth);
+
+    const handleSignUp = (e) => {
+        e.preventDefault();
+        if(password !== confirmPassword) {
+            return setDisplayError('Invalid! Password not matched.')
+        } else {
+            setDisplayError('')
+        }
+
+        createUserWithEmailAndPassword(email, password);
+    };
+
+    if(user) {
+        navigate('/');
+    }
+
     return (
         <div className="form-container">
             <div className="form-heading">
                 <h2>Sign Up</h2>
             </div>
-            <form>
+            <form onSubmit={handleSignUp}>
                 <div className="field-group">
                     <label htmlFor="email">Email</label>
                     <input
+                        onBlur={(e) => setEmail(e.target.value)}
                         type="email"
                         name="email"
                         placeholder="Enter your email"
                         id="email"
+                        required
                     />
                 </div>
                 <div className="field-group">
                     <label htmlFor="password">Password</label>
                     <input
+                        onBlur={(e) => setPassword(e.target.value)}
                         type="password"
                         name="password"
                         placeholder="Enter your password"
                         id="password"
+                        required
                     />
                 </div>
                 <div className="field-group">
                     <label htmlFor="confirm-password">Password</label>
                     <input
-                        type="confirm-password"
+                        onBlur={(e) => setConfirmPassword(e.target.value)}
+                        type="password"
                         name="confirm-password"
                         placeholder="Re enter your password"
                         id="confirm-password"
+                        required
                     />
+                </div>
+                <div className="error">
+                    <p>{displayError}</p>
                 </div>
                 <div className="field-group">
                     <input type="submit" value="Login" />
